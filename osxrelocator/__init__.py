4176ecba54e7958e5c1706dddd56314217f50d3b
akshayaurora <akshayaurora@gmail.com>
2015-12-08 01:15:01 +0530

    fixes for python3

--- osxrelocator/__init__.py
+++ osxrelocator/__init__.py
@@ -26,7 +26,7 @@ OTOOL_CMD = 'otool'
 
 
 def shell_call(cmd, cmd_dir='.', fail=True):
-    print "call", cmd
+    print("call", cmd)
     try:
         ret = subprocess.check_call(
                 cmd, cwd=cmd_dir,
@@ -40,7 +40,7 @@ def shell_call(cmd, cmd_dir='.', fail=True):
 
 
 def shell_check_call(cmd):
-    print "ccall", cmd
+    print("ccall", cmd)
     try:
         process = subprocess.Popen(
                 cmd, stdout=subprocess.PIPE)
@@ -62,8 +62,8 @@ class OSXRelocator(object):
 
     def __init__(self, root, lib_prefix, new_lib_prefix, recursive):
         self.root = root
-        self.lib_prefix = self._fix_path(lib_prefix)
-        self.new_lib_prefix = self._fix_path(new_lib_prefix)
+        self.lib_prefix = self._fix_path(lib_prefix).encode('utf-8')
+        self.new_lib_prefix = self._fix_path(new_lib_prefix).encode('utf-8')
         self.recursive = recursive
 
     def relocate(self):
@@ -74,7 +74,7 @@ class OSXRelocator(object):
         self.change_id(object_file, id)
 
     def change_id(self, object_file, id=None):
-        id = id or object_file.replace(self.lib_prefix, self.new_lib_prefix)
+        id = id or object_file.replace(self.lib_prefix.decode('utf-8'), self.new_lib_prefix.decode('utf-8'))
         filename = os.path.basename(object_file)
         if not (filename.endswith('so') or filename.endswith('dylib')):
             return
@@ -106,13 +106,13 @@ class OSXRelocator(object):
     @staticmethod
     def list_shared_libraries(object_file):
         cmd = [OTOOL_CMD, "-L", object_file]
-        res = shell_check_call(cmd).split('\n')
+        res = shell_check_call(cmd).split(b'\n')
         # We don't use the first line
         libs = res[1:]
         # Remove the first character tabulation
         libs = [x[1:] for x in libs]
         # Remove the version info
-        libs = [x.split(' ', 1)[0] for x in libs]
+        libs = [x.split(b' ', 1)[0] for x in libs]
         return libs
 
     @staticmethod
